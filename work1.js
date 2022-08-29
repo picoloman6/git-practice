9573ae6bf2d70cf1c8e076288f851797a2a0f6a6
picoloman6 <85938399+picoloman6@users.noreply.github.com>
2022-08-29 15:05:22 +0900

    second

--- work1.js
+++ work1.js
@@ -1,3 +1,7 @@
-const say = str => {
-    console.log(str)
+const say = (str) => {
+  console.log(str);
+};
+
+const add = (...numbers) => {
+  return numbers.reduce((cur, next) => cur + next, 0);
 };
